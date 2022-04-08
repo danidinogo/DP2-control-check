ca5104f742d03256f3e7f0969ce7f4a9b7b7a83c
luibelzan <luismi_madrilista@hotmail.com>
2022-04-08 21:39:34 +0200

    Model fixed

--- src/main/java/acme/entities/patronage/Patronage.java
+++ src/main/java/acme/entities/patronage/Patronage.java
@@ -1,11 +1,9 @@
 package acme.entities.patronage;
 import java.util.Date;
-import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
@@ -16,7 +14,6 @@ import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.URL;
 
-import acme.entities.patronagereport.PatronageReport;
 import acme.enums.Status;
 import acme.framework.datatypes.Money;
 import acme.framework.entities.AbstractEntity;
@@ -65,7 +62,5 @@ public class Patronage extends AbstractEntity {
 	@Valid
 	protected Inventor inventor;
 	
-	@OneToMany
-	protected List<PatronageReport> patronageReports;
 	
 }
