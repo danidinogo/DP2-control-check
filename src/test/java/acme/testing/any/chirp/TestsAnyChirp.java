b463c37f9c2b7b0a59e71950a9eb4a3ea0d32745
Josemarii26 <josemarigquijada@gmail.com>
2022-04-25 12:50:08 +0200

    Test1.1 Task003 Done

--- src/test/java/acme/testing/any/chirp/TestsAnyChirp.java
+++ src/test/java/acme/testing/any/chirp/TestsAnyChirp.java
@@ -11,18 +11,18 @@ public class TestsAnyChirp extends TestHarness {
 	@ParameterizedTest
 	@CsvFileSource(resources = "/any/chirp/chirp.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(10)
-	public void testAnyChirpList(final int key, final String version,final String author, final String body, 
+	public void testAnyChirpList(final int recordIndex, final String version,final String author, final String body, 
 		final String creationTime, final String email, final String title ) {
 
 		
 		super.clickOnMenu("Anonymous","Chirps");
 		super.checkListingExists();
 		
-		super.checkColumnHasValue(key, 0, author);
-		super.checkColumnHasValue(key, 1, body);
-		super.checkColumnHasValue(key, 2, creationTime);
-		super.checkColumnHasValue(key, 3, email);
-		super.checkColumnHasValue(key, 4, title);
+		super.checkColumnHasValue(recordIndex, 0, author);
+		super.checkColumnHasValue(recordIndex, 1, body);
+		super.checkColumnHasValue(recordIndex, 2, creationTime);
+		super.checkColumnHasValue(recordIndex, 3, email);
+		super.checkColumnHasValue(recordIndex, 4, title);
 
 	}
 }
